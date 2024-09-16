978409062cea57ee074479105d868173f888847e
Daniel Jorge Manzano <djorgemanzano@gmail.com>
2024-09-16 13:03:49 -0300

    Init

--- exerciciosaula/buscadivide.c
+++ exerciciosaula/buscadivide.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-//o exercicio é achar um numero dado que ta dentro de um vetor (ja ordenado) com uma funcao recursiva
-//essa funcao fica dividindo o vetor no meio e vai procurar o numero na metade que tem teoricamente o valor mais proximo ao que voce ta procurando
-//começarei eventualmente, esse .c é só pra eu nao esquecer
+
+/*o exercicio é achar um numero dado que ta dentro de um vetor (ja ordenado) com uma funcao recursiva*/
+/*o problema é basico, mas o legal aqui é que esse tipo de busca é muuuuito bom (complexidade O(logn), log na base 2)*/
+/*vou reutilizar o algoritmo em exercicio de alg*/
 
 int buscabin(int *vet, int i, int f, int busca){
     int m = (i + f) / 2;
@@ -20,9 +21,16 @@ int buscabin(int *vet, int i, int f, int busca){
 }
 
 int main(void){
-    int n;
+    int tam, n;
+    printf("escreva o tamanho do seu vetor ");
+    scanf("%d", &tam);
+    int *vet = malloc(sizeof(int) * tam); 
+
+    printf("escreva seu vetor (tem de ser ordenado!) ");
+    for(int i = 0; i < tam; i++) scanf(" %d", &vet[i]);
+
+    printf("escreva o numero que quer buscar ");
     scanf("%d", &n);
 
-    int vet[] = {1, 2, 3, 4, 5, 6, 7};
-    printf("%d\n", buscabin(vet, 0, 6, n));
+    printf("o indice do numero no vetor é %d\n", buscabin(vet, 0, tam - 1, n));
 }
