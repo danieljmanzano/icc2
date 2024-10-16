af6d58f77f4b809db59b647950324e3170b6a7b6
Daniel Jorge Manzano <djorgemanzano@gmail.com>
2024-10-16 19:57:17 -0300

    arrumei o stogronofe

--- cartas/stooge/cartas.c
+++ cartas/stooge/cartas.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 //feito pelo daniel
 //exercicio de lab de icc2. temos que ordenar cartas de baralho (truco) com o stoogesort (pior algoritmo possivel)
-//curiosamente a carta tem mais de um dígito de numero/letra, aí tem que ordenar com base nisso tambem. depois disso ainda temos que ordenar no naipe
-//como o stoogesort, alias, nao é estavel, quando ordeno por naipe muito provavelmente ele tira as cartas de ordem 
-//assim, esse algoritmo é terrivel e nao deve ser usado quando compara com o outro da aula
+//curiosamente a carta tem mais de um dígito de numero/letra, aí tem que ordenar primeiro pelo naipe e depois com base nisso 
+
+
+/*daqui pra baixo é pra funcionar o timer*/
+// Struct para o funcionamento do timer
+typedef struct {
+    clock_t start;
+    clock_t end;
+} Timer;
+
+// Inicializa o timer
+void start_timer(Timer *timer) {
+    timer->start = clock();
+}
+
+// Para o timer e calcula o tempo decorrido
+double stop_timer(Timer *timer) {
+    timer->end = clock();
+    return ((double) (timer->end - timer->start)) / CLOCKS_PER_SEC;
+}
+
+void imprimeTempoDeExecucao(double tempo){
+    printf("tempo de execução: %f segundos\n", tempo);
+}
+/*daqui pra frente é o codigo de vdd*/
+
 
 typedef struct{
     char *naipe; //naipe é um caracter especial, funciona como uma string pra garantir que cabe aí
@@ -34,32 +58,31 @@ void swap(carta *a, carta *b){
     return;
 }
 
-void stoogesortnum(carta *vet, int i, int j, int seq[], int tam, int n){    
-    int k, flag = 0;
-    
-    if(buscaemseq(seq, vet[i].num[0]) > buscaemseq(seq, vet[j].num[0]))
-        swap(&vet[i], &vet[j]);
-    else{
-        for(int k = 1; k < tam; k++){ //vou analisar todos os digitos do numero da carta
-            if(buscaemseq(seq, vet[i].num[k]) > buscaemseq(seq, vet[j].num[k])){
-                swap(&vet[i], &vet[j]);
-                flag = 1; //essa é pra ver se vou ter q analisar o naipe dps
-                break;
-            }
-        }
-        if(!flag){ //essa é pra se o numero foi todo igual e tenho que analisar o naipe
-            if(buscanaipe(vet[i].naipe) > buscanaipe(vet[j].naipe))
-                swap(&vet[i], &vet[j]);
-        }
+int comparacarta(carta *vet, int i, int j, int seq[], int tam){ //aqui vou ver se preciso trocar duas cartas (1 é que tem que trocar, 0 não)
+    if(buscanaipe(vet[i].naipe) > buscanaipe(vet[j].naipe)) return 1;
+    else if(buscanaipe(vet[i].naipe) < buscanaipe(vet[j].naipe)) return 0;
+
+    for(int cont = 0; cont <= tam; cont++){ //esse for é pra ver dentre os numeros se eu preciso trocar os numeros ou nao
+        if(buscaemseq(seq, vet[i].num[cont]) > buscaemseq(seq, vet[j].num[cont])) //se for menor troca
+            return 1;
+        else if(buscaemseq(seq, vet[i].num[cont]) < buscaemseq(seq, vet[j].num[cont])) //se for maior nao
+            return 0;
     }
 
+    return 0; //por via das duvidas, se a carta for igual eu nao troco
+}
+
+void stoogesort(carta *vet, int i, int j, int seq[], int tam){ 
+    if(comparacarta(vet, i, j, seq, tam))
+        swap(&vet[i], &vet[j]);
+    
     if((i + 1) >= j) return;
 
-    k = (int)((j - i + 1) / 3);
+    int k = ((j - i + 1) / 3);
 
-    stoogesortnum(vet, i, j - k, seq, tam, n);
-    stoogesortnum(vet, i + k, j, seq, tam, n);
-    stoogesortnum(vet, i, j - k, seq, tam, n);
+    stoogesort(vet, i, j - k, seq, tam);
+    stoogesort(vet, i + k, j, seq, tam);
+    stoogesort(vet, i, j - k, seq, tam);
 }
 
 void printa(carta *vet, int n){
@@ -68,18 +91,21 @@ void printa(carta *vet, int n){
 }
 
 int main(void){ 
+    Timer timer;
     int n, tam; //n == numero de cartas, tam == tamanho do numero da carta (o numero da carta tem mais de um digito, nada ve)
     int seq[10] = {52, 53, 54, 55, 81, 74, 75, 65, 50, 51}; //a ordem convencional de baralho de truco so que ta tudo em ascii
     scanf("%d %d", &n, &tam);
     carta *cartas = malloc(sizeof(carta) * n);
     for(int i = 0; i < n; i++){ //pra entrada
-        cartas[i].naipe = malloc(sizeof(carta) * 2);
-        cartas[i].num = malloc(sizeof(cartas) * (n + 1));
+        cartas[i].naipe = malloc(sizeof(char) * 3);
+        cartas[i].num = malloc(sizeof(char) * (tam + 1));
         scanf(" %s %s", cartas[i].naipe, cartas[i].num);
     }
     
-    stoogesortnum(cartas, 0, n - 1, seq, tam - 1, n);
+    start_timer(&timer);
+    stoogesort(cartas, 0, n - 1, seq, tam - 1);
     printa(cartas, n);
+    imprimeTempoDeExecucao(stop_timer(&timer));
 
     return 0;
 }
